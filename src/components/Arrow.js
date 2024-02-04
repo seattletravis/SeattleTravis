dc9f8fb0aae06236e1d66c1292147b9ddb4290ab
Travis Lamberte <travis@lamberte.com>
2024-02-04 13:26:21 -0800

    change styles

--- src/components/Arrow.js
+++ src/components/Arrow.js
@@ -1,30 +1,24 @@
-import '../App.css'
+import '../App.css';
 
-export default function Arrow({
-  children,
-  disabled,
-  onClick
-}) {
-  return (
-    <button
-      disabled={disabled}
-      onClick={onClick}
-      style={{
-        cursor: "pointer",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        right: "1%",
-        opacity: disabled ? "0" : "1",
-        border: "black",
-        userSelect: "none",
-        color: "white",
-        fontSize: "5vw",
-        backgroundColor: "black"
-      }}
-    >
-      {children}
-    </button>
-  );
+export default function Arrow({ children, disabled, onClick }) {
+	return (
+		<button
+			disabled={disabled}
+			onClick={onClick}
+			style={{
+				cursor: 'pointer',
+				display: 'flex',
+				flexDirection: 'column',
+				justifyContent: 'center',
+				right: '1%',
+				opacity: disabled ? '0' : '1',
+				border: 'black',
+				userSelect: 'none',
+				color: 'white',
+				fontSize: '5vw',
+				backgroundColor: '#393A3F',
+			}}>
+			{children}
+		</button>
+	);
 }
-
--- src/components/RightArrow.js
+++ src/components/RightArrow.js
@@ -1,21 +1,22 @@
 import Arrow from './Arrow';
 import React, { useContext } from 'react';
 import { VisibilityContext } from 'react-horizontal-scrolling-menu';
-import { BsFillCaretRightFill } from 'react-icons/bs'
+import { BsFillCaretRightFill } from 'react-icons/bs';
 import useScreenSmall from '../hooks/useScreenSmall';
 
 //styles
-import '../App.css'
+import '../App.css';
 
 export default function RightArrow() {
+	const isSmall = useScreenSmall();
 
-  const isSmall = useScreenSmall()
+	const { isLastItemVisible, scrollNext } = useContext(VisibilityContext);
 
-  const { isLastItemVisible, scrollNext } = useContext(VisibilityContext);
-
-  return (
-    <Arrow disabled={isLastItemVisible} onClick={() => scrollNext()}>
-      <BsFillCaretRightFill className={isSmall ? 'arrowsSmallScreen' : 'arrows'}/>
-    </Arrow>
-  );
+	return (
+		<Arrow disabled={isLastItemVisible} onClick={() => scrollNext()}>
+			<BsFillCaretRightFill
+				className={isSmall ? 'arrowsSmallScreen' : 'arrows'}
+			/>
+		</Arrow>
+	);
 }
