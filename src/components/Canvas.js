ac019be9d7ce14e2b403c7ea91d1aca05599e740
Travis Lamberte <travis@lamberte.com>
2024-03-01 12:30:54 -0800

    fix canvas issue

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -4,10 +4,13 @@ const Canvas = (props) => {
 	const canvasRef = useRef(null);
 
 	const draw = (ctx, frameCount) => {
+		ctx.canvas.width = window.innerWidth;
+		ctx.canvas.height = window.innerHeight;
 		ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
 		ctx.fillStyle = '#000000';
 		ctx.beginPath();
-		ctx.arc(250, 150, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI);
+		ctx.arc(550, 480, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI);
 		ctx.fill();
 	};
 
