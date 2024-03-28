4c3103b14cbe50dea3c7f3c60be1d81b9cfdf4f3
Travis Lamberte <travis@lamberte.com>
2024-03-28 07:30:26 -0700

    update section

--- src/components/Update.js
+++ src/components/Update.js
@@ -7,7 +7,9 @@ function Update({ imagePath }) {
 				<thead>
 					<tr>
 						<th>Updates:</th>
-						<td>(Jan 2024): Enrolled in IBM Full-Stack Developers Bootcamp!</td>
+						<td>
+							(Mar 2024): I recieved the IBM Full-Stack Developer Certificate!
+						</td>
 						<td>(Dec 2023): I received the Google IT Support Certificate!</td>
 						<td>
 							(Oct 2023): I received the Meta Front-End Developer Certificate!
